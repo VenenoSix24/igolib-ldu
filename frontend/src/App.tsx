b854fc83839d87380ed3c077212e0fc8cf31923f
VenenoSix24 <3405395460@qq.com>
2026-01-24 03:09:02 +0800

    fix: 调整默认主题设置为跟随系统

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
