41c299ecb9bbe729e2016df3f6684d63c7366f26
VenenoSix24 <3405395460@qq.com>
2026-01-28 16:10:54 +0800

    refactor: 隔离桌面端窗口逻辑

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,20 +2,22 @@ use tauri::Manager;
 
 #[tauri::command]
 async fn close_splashscreen(app: tauri::AppHandle) {
-    if let Some(splash_window) = app.get_webview_window("splashscreen") {
-        splash_window.close().unwrap();
-    }
+    #[cfg(not(mobile))]
+    {
+        if let Some(splash_window) = app.get_webview_window("splashscreen") {
+            splash_window.close().unwrap();
+        }
 
-    if let Some(main_window) = app.get_webview_window("main") {
-        main_window.show().unwrap();
-        main_window.set_focus().unwrap();
+        if let Some(main_window) = app.get_webview_window("main") {
+            main_window.show().unwrap();
+            main_window.set_focus().unwrap();
+        }
     }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![close_splashscreen])
         .setup(|app| {
